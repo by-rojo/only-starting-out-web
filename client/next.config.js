9a62e65667e2190fdadae3e386784bd59e4c4750
Roe Greene <robert@byrojo.com>
2022-07-17 12:56:51 -0700

    add styles

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -13,6 +15,10 @@ const nextConfig = {
     })
 
     return config
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    additionalData: '@import "variables";',
   },
 }
 
