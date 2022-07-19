99af2dee46588fc5d948e5a8719b22eaac4d32db
Roe Greene <robert@byrojo.com>
2022-07-19 01:55:28 -0700

    add how it works section

--- client/components/get-started-page/how-it-works/index.tsx
+++ client/components/get-started-page/how-it-works/index.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { HOW_IT_WORKS_LIST } from './constants'
+
 const HowItWorks: React.FC = () => {
   return (
     <div className="container my-5">
@@ -11,6 +15,44 @@ const HowItWorks: React.FC = () => {
         Sometimes all you need is your start. Let us connect you with the
         perfect one.
       </p>
+      <div className="d-flex flex-wrap mx-n3 mt-5">
+        {HOW_IT_WORKS_LIST.map((item) => (
+          <div key={item.title} className="col-12 col-lg-6 d-flex">
+            <div className="card m-3 shadow-sm">
+              <div className="row g-0 h-100">
+                <div className="col-4 position-relative">
+                  <Image
+                    src={item.image.src}
+                    alt={item.image.alt}
+                    objectFit="cover"
+                    layout="fill"
+                  />
+                </div>
+                <div className="col-8">
+                  <div className="card-body">
+                    <h5 className="card-title text-primary fw-bold">
+                      {item.title}
+                    </h5>
+                    <p className="card-text">{item.description}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="d-flex justify-content-center flex-wrap mt-4">
+        <Link href="/register" passHref>
+          <a className="btn btn-dark col-12 col-lg-3 col-md-4">
+            Join our program
+          </a>
+        </Link>
+        <Link href="/faq" passHref>
+          <a className="btn btn-info col-12 col-lg-3 ms-md-3 mt-3 mt-sm-auto col-md-4">
+            Frequently asked questions
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
