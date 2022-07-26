38fc22df197cc012922bbf376dc4ce1101154803
Roe Greene <robert@byrojo.com>
2022-07-26 00:28:45 -0700

    add button to search bar

--- client/components/find-projects-page/search-filter/index.tsx
+++ client/components/find-projects-page/search-filter/index.tsx
@@ -4,11 +4,11 @@ import SearchIcon from './magnifying-glass.svg'
 
 const SearchFilter: React.FC = () => {
   return (
-    <InputGroup
-      suffix={<SearchIcon width="1rem" height="1rem" color="white" />}
-      primary
-    >
+    <InputGroup primary>
       <Input />
+      <button className="input-group-text bg-primary text-white btn btn-primary">
+        <SearchIcon width="1rem" height="1rem" />
+      </button>
     </InputGroup>
   )
 }
